0b3024db1554d44279c273dd9cbc68d6417260e2
Iftakharul Alam <bappa2du@gmail.com>
2020-10-23 18:26:06 +0600

    init

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import {connect} from 'react-redux';
+import updateMovies from './store/actions/updateMovies';
+import fetchUsers from "./store/actions/fetchUsers";
+
+function App(props) {
+    return (
+        <div className="App"><h3>REDUX MOVIELIST APP</h3><br/>
+            <span style={{color: 'green'}}>YOUR CURRENT MOVIE IS:</span>
+            {props.movies.name}<br/>
+            <p>
+                <button onClick={props.updateMovies}>SELECT NEW MOVIE</button>
+            </p>
+            <br/>
+            {props.users.length === 0 ? <p>THERE ARE NO USERS</p> : props.users.map(user => <p
+                key={user.id}>{user.first_name} - {user.email}</p>)}
+
+            <br/>
+            <button onClick={props.fetchUsers}>FETCH USERS</button>
+        </div>
+    );
+}
+
+const MapStateToProps = (state) => {
+    return {
+        movies: state.movies,
+        users: state.users
+    };
+}
+
+const MapDispatchToProps = (dispatch) => {
+    return {
+        updateMovies: () => dispatch(updateMovies),
+        fetchUsers: () => dispatch(fetchUsers)
+    }
+};
+export default connect(MapStateToProps, MapDispatchToProps)(App);
---
+++ src/store/actions/updateMovies.js
@@ -0,0 +1,3 @@
+export const UPDATE_MOVIES = "UPDATE_MOVIES";
+const updateMovies = {type: UPDATE_MOVIES, payload: "THE BREAKFAST CLUB"};
+export default updateMovies;
